06366b730351550566fdafb3aed3af0587eb3e8d
TranThiKimNghi <161545432+TranThiKimNghi@users.noreply.github.com>
2024-02-29 10:44:55 +0700

    Add files via upload

---
+++ person.java
@@ -0,0 +1,41 @@
+public class person {
+    private int id;
+    private String name;
+    private int age;
+    public person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public static void main(String[] args) {
+        Person person1 = new Person("Nghi", 20);
+        Person person2 = new Person("Mi", 21);
+        System.out.println("Person 1: Name = " + person1.getName() + ", Age = " + person1.getAge());
+        System.out.println("Person 2: Name = " + person2.getName() + ", Age = " + person2.getAge());
+    }
+
+}
